0fd9741413633408589593766bb2f96262d71575
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-03-05 10:54:43 +0100

    Fix sonar issues

--- src/main/java/sonia/scm/redmine/config/RedmineConfigurationResource.java
+++ src/main/java/sonia/scm/redmine/config/RedmineConfigurationResource.java
@@ -39,12 +39,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import sonia.scm.config.ConfigurationPermissions;
 import sonia.scm.redmine.Constants;
 import sonia.scm.redmine.RedmineIssueTracker;
@@ -65,9 +62,6 @@ public class RedmineConfigurationResource {
   private RedmineIssueTracker tracker;
   private RedmineConfigurationMapper mapper;
   private RepositoryManager repositoryManager;
-
-  private static final Logger logger =
-    LoggerFactory.getLogger(RedmineConfigurationResource.class);
 
   @Inject
   public RedmineConfigurationResource(RedmineIssueTracker tracker, RedmineConfigurationMapper mapper, RepositoryManager repositoryManager) {
