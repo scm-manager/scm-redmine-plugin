50645dd1616c9138beb1e3aba223dbf8c0a82854
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-06-21 11:10:08 +0200

    Use test support from core

--- src/test/java/sonia/scm/redmine/update/RedmineV2ConfigMigrationUpdateStepTest.java
+++ src/test/java/sonia/scm/redmine/update/RedmineV2ConfigMigrationUpdateStepTest.java
@@ -11,32 +11,23 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import sonia.scm.redmine.config.RedmineConfigStore;
 import sonia.scm.redmine.config.RedmineGlobalConfiguration;
 import sonia.scm.redmine.config.TextFormatting;
-import sonia.scm.update.MapBasedPropertyReaderInstance;
-import sonia.scm.update.RepositoryV1PropertyReader;
-import sonia.scm.update.V1Properties;
-import sonia.scm.update.V1Property;
-import sonia.scm.update.V1PropertyDAO;
-import sonia.scm.update.V1PropertyReader;
+import sonia.scm.update.V1PropertyDaoTestUtil;
+import sonia.scm.update.V1PropertyDaoTestUtil.PropertiesForRepository;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-import static java.util.Arrays.stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RedmineV2ConfigMigrationUpdateStepTest {
 
-  @Mock
-  V1PropertyDAO v1PropertyDAO;
+  V1PropertyDaoTestUtil testUtil = new V1PropertyDaoTestUtil();
+
   @Mock
   RedmineConfigStore configStore;
 
@@ -53,7 +44,7 @@ class RedmineV2ConfigMigrationUpdateStepTest {
 
   @BeforeEach
   void initUpdateStep() {
-    updateStep = new RedmineV2ConfigMigrationUpdateStep(v1PropertyDAO, configStore);
+    updateStep = new RedmineV2ConfigMigrationUpdateStep(testUtil.getPropertyDAO(), configStore);
   }
 
   @Test
@@ -67,7 +58,7 @@ class RedmineV2ConfigMigrationUpdateStepTest {
         "redmine.auto-close", "true"
       );
 
-    mockRepositoryProperties(new PropertiesForRepository("repo", mockedValues));
+    testUtil.mockRepositoryProperties(new PropertiesForRepository("repo", mockedValues));
 
     updateStep.doUpdate();
 
@@ -87,31 +78,10 @@ class RedmineV2ConfigMigrationUpdateStepTest {
         "any", "value"
       );
 
-    mockRepositoryProperties(new PropertiesForRepository("repo", mockedValues));
+    testUtil.mockRepositoryProperties(new PropertiesForRepository("repo", mockedValues));
 
     updateStep.doUpdate();
 
     verify(configStore, never()).storeConfiguration(any(), eq("repo"));
-  }
-
-  protected void mockRepositoryProperties(PropertiesForRepository... mockedPropertiesForRepositories) {
-    Map<String, V1Properties> map = new HashMap<>();
-    stream(mockedPropertiesForRepositories).forEach(p -> map.put(p.repositoryId, p.asProperties()));
-    V1PropertyReader.Instance v1PropertyReader = new MapBasedPropertyReaderInstance(map);
-    when(v1PropertyDAO.getProperties(argThat(argument -> argument instanceof RepositoryV1PropertyReader))).thenReturn(v1PropertyReader);
-  }
-
-  protected static class PropertiesForRepository {
-    private final String repositoryId;
-    private final Map<String, String> properties;
-
-    public PropertiesForRepository(String repositoryId, Map<String, String> properties) {
-      this.repositoryId = repositoryId;
-      this.properties = properties;
-    }
-
-    V1Properties asProperties() {
-      return new V1Properties(properties.entrySet().stream().map(e -> new V1Property(e.getKey(), e.getValue())).collect(Collectors.toList()));
-    }
   }
 }
